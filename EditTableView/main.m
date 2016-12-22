523a0096274597ed95beac6ce15f216c4b994b74
guoda <1023251767@qq.com>
2016-12-22 09:54:31 +0800

    Initial Commit

---
+++ EditTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EditTableView
+//
+//  Created by X-Designer on 16/12/22.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
